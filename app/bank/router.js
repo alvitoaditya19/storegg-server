37a282d6e085d5390a24195f5d8668eeb1bcb46b
MuhammadAlvitoAditya <itoa47@gmail.com>
2022-03-03 11:44:12 +0700

    upload github

---
+++ app/bank/router.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var router = express.Router();
+var {
+  index,
+  viewCreate,
+  actionCreate,
+  viewEdit,
+  actionEdit,
+  actionDelete,
+} = require("./controller");
+
+const { isLoginAdmin } = require("../middleware/auth");
+
+/* GET home page. */
+router.use(isLoginAdmin);
+
+
+/* GET home page. */
+router.get("/", index);
+router.get("/create", viewCreate);
+router.post("/create", actionCreate);
+router.get("/edit/:id", viewEdit);
+router.put("/edit/:id", actionEdit);
+
+router.delete("/delete/:id", actionDelete);
+
+module.exports = router;
---
+++ app/payment/model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+let paymentSchema = mongoose.Schema({
+  type: {
+    type: String,
+    require: [true, "Type Pembayaran Harus Diisi"],
+  },
+  status: {
+    type: String,
+    enum: ["Y", "N"],
+    default: "Y",
+  },
+  banks: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Bank",
+    },
+  ],
+},{ timestamps: true });
+
+module.exports = mongoose.model("Payment", paymentSchema);
